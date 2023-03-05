a62dfaf36804052202bb735a8dc64a3883eee0ca
d0wnsider <jmhandsamson@gmail.com>
2023-03-04 23:12:53 -0500

    Refactor with basic functionality

--- calculator.js
+++ calculator.js
@@ -1,24 +1,11 @@
-let display = document.getElementById('display');
-let displayResult = document.getElementById('display-result');
-let chosenOperator = null;
-// const operateSelect = document.querySelector('#equals');
-// const addSelect = document.querySelector('#add');
-const subtractSelect = document.querySelector('#subtract');
-// const multiplySelect = document.querySelector('#multiply');
-// const divideSelect = document.querySelector('#divide');
-const clear = document.querySelector('#clear');
-const zero = document.querySelector('#zero');
-// const one = document.querySelector('#one');
-// const two = document.querySelector('#two');
-// const three = document.querySelector('#three');
-// const four = document.querySelector('#four');
-// const five = document.querySelector('#five');
-// const six = document.querySelector('#six');
-// const seven = document.querySelector('#seven');
-// const eight = document.querySelector('#eight');
-// const nine = document.querySelector('#nine');
+let operand1 = document.getElementById('display');
+let operand2 = document.getElementById('display-result');
+let dNum = 0;
+let rNum = 0;
+let result = 0;
+let textNum = '';
+let chosenOperator = '';
 const numberContainer = document.querySelector('.number-container');
-const remove = new AbortController(); // Disabling the effect of listener
 // Operations 
 const add = (a,b) => a + b;
 const subtract = (a,b) => a - b;
@@ -28,189 +15,92 @@ const divide = (a,b) => a / b;
 function operate(operator,a,b) {
     if (operator === 'add') {
         return add(a,b);
-    }
-    if (operator === 'subtract') {
+    }if (operator === 'subtract') {
         return subtract(a,b);
-    }
-    if (operator === 'multiply') {
+    }if (operator === 'multiply') {
         return multiply(a,b);
-    }
-    if (operator === 'divide') {
+    }if (operator === 'divide') {
         return divide(a,b);
     }
-};
+}
+function display(num) {
+    return operand1.textContent += num;
+}
+function display2(a) {
+    operand2.textContent += textNum + a;
+}
+function clear() {
+    dNum = 0;
+    rNum = 0;
+    textNum = '';
+    chosenOperator = '';
+    operand1.textContent = '';
+    operand2.textContent = '';
+}
 // Event listener on container rather than per button
 // Event delegation!
 numberContainer.addEventListener('click', (e) => {
-    // displaying the result
-    if (e.target.matches('#equals')) {
-        if (chosenOperator === 'add'){
-            display.textContent = operate(chosenOperator,Number(displayResult.textContent),Number(display.textContent));
+    // Displaying the result
+    const target = e.target;
+    if (target.matches('#zero')) {
+        textNum = display(0);
+    } if (target.matches('#one')) {
+        textNum = display(1);
+    } if (target.matches('#two')) {
+        textNum = display(2);
+    } if (target.matches('#three')) {
+        textNum = display(3);
+    } if (target.matches('#four')) {
+        textNum = display(4);
+    } if (target.matches('#five')) {
+        textNum = display(5);
+    } if (target.matches('#six')) {
+        textNum = display(6);
+    } if (target.matches('#seven')) {
+        textNum = display(7);
+    } if (target.matches('#eight')) {
+        textNum = display(8);
+    } if (target.matches('#nine')) {
+        textNum = display(9);
+    } if (target.matches('#clear')) {
+        clear();
+    } if (target.matches('#equals')) {
+        rNum = Number(textNum);
+        result = operate(chosenOperator,dNum,rNum);
+        operand2.textContent += rNum; // adding 2nd number to 2nd display
+        operand1.textContent = '';
+        console.log(chosenOperator,dNum,rNum,result);
+        display(result);
+    } if (target.matches('#add')) {
+        // should not evaluate more than a single pair of numbers how??? check if operator is not empty??
+        if (chosenOperator !== '') {
+            rNum = Number(textNum);
+            result = operate(chosenOperator,dNum,rNum);
+            operand2.textContent = result;
+            dNum = result;
+            console.log(dNum)
+            chosenOperator = '';
+            operand1.textContent = '';
+        } else if (chosenOperator === '') {
+            chosenOperator = 'add';
+            dNum = Number(textNum);
+            operand2.textContent += textNum + ' + ';
+            operand1.textContent = '';
         }
-        if (chosenOperator === 'subtract'){
-            display.textContent = operate(chosenOperator,Number(displayResult.textContent),Number(display.textContent));
-        }
-        if (chosenOperator === 'multiply'){
-            display.textContent = operate(chosenOperator,Number(displayResult.textContent),Number(display.textContent));
-        }
-        if (chosenOperator === 'divide'){
-            display.textContent = operate(chosenOperator,Number(displayResult.textContent),Number(display.textContent));
-        }
-    }
-
-    if (e.target.matches('#add')) {
-        if (display.textContent === '0') {
-            display.textContent += display.textContent;
-        }
-        displayResult.textContent = display.textContent;
-        if (!display.textContent === '0') {
-            display.textContent += displayResult;
-        }
-        display.textContent = 0;
-        chosenOperator = 'add';
-    }
-    if (e.target.matches('#subtract')) {
-        if (display.textContent === '0') {
-            display.textContent = display.textContent;
-        }
-        displayResult.textContent = display.textContent;
-        if (!display.textContent === '0') {
-            display.textContent += displayResult;
-        }
-        display.textContent = 0;
+    } if (target.matches('#subtract')) {
         chosenOperator = 'subtract';
-    }
-    if (e.target.matches('#multiply')) {
-        if (display.textContent === '0') {
-            display.textContent = display.textContent;
-        }
-        displayResult.textContent = display.textContent;
-        if (!display.textContent === '0') {
-            display.textContent += displayResult;
-        }
-        display.textContent = 0;
+        dNum = Number(textNum)
+        operand2.textContent += textNum + ' - ';
+        operand1.textContent = '';
+    } if (target.matches('#multiply')) {
         chosenOperator = 'multiply';
-    }
-    if (e.target.matches('#divide')) {
-        if (display.textContent === '0') {
-            display.textContent = display.textContent;
-        }
-        displayResult.textContent = display.textContent;
-        if (!display.textContent === '0') {
-            display.textContent += displayResult;
-        }
-        display.textContent = 0;
+        dNum = Number(textNum)
+        operand2.textContent += textNum + ' * ';
+        operand1.textContent = '';
+    } if (target.matches('#divide')) {
         chosenOperator = 'divide';
-    }
-
-    if (e.target.matches('#one')) {
-        if (display.textContent === '0') {
-            display.textContent = 1;
-        }
-        else if (display.textContent.length < 12 && display.textContent.length > 0) {
-            display.textContent += 1;
-        }
-        if (display.textContent.length > 12) {
-            remove.abort;
-        }
-    }
-    if (e.target.matches('#two')) {
-        if (display.textContent === '0') {
-            display.textContent = 2;
-        }
-        else if (display.textContent.length < 12 && display.textContent.length > 0) {
-            display.textContent += 2;
-        }
-        else {
-            remove.abort;
-        }
-    }
-    if (e.target.matches('#three')) {
-        if (display.textContent === '0') {
-            display.textContent = 3;
-        }
-        else if (display.textContent.length < 12 && display.textContent.length > 0) {
-            display.textContent += 3;
-        }
-        else {
-            remove.abort;
-        }
-    }
-    if (e.target.matches('#four')) {
-        if (display.textContent === '0') {
-            display.textContent = 4;
-        }
-        else if (display.textContent.length < 12 && display.textContent.length > 0) {
-            display.textContent += 4;
-        }
-        else {
-            remove.abort;
-        }
-    }
-    if (e.target.matches('#five')) {
-        if (display.textContent === '0') {
-            display.textContent = 5;
-        }
-        else if (display.textContent.length < 12 && display.textContent.length > 0) {
-            display.textContent += 5;
-        }
-        else {
-            remove.abort;
-        }
-    }
-    if (e.target.matches('#six')) {
-        if (display.textContent === '0') {
-            display.textContent = 6;
-        }
-        else if (display.textContent.length < 12 && display.textContent.length > 0) {
-            display.textContent += 6;
-        }
-        else {
-            remove.abort;
-        }
-    }
-    if (e.target.matches('#seven')) {
-        if (display.textContent === '0') {
-            display.textContent = 7;
-        }
-        else if (display.textContent.length < 12 && display.textContent.length > 0) {
-            display.textContent += 7;
-        }
-        else {
-            remove.abort;
-        }
-    }
-    if (e.target.matches('#eight')) {
-        if (display.textContent === '0') {
-            display.textContent = 8;
-        }
-        else if (display.textContent.length < 12 && display.textContent.length > 0) {
-            display.textContent += 8;
-        }
-        else {
-            remove.abort;
-        }
-    }
-    if (e.target.matches('#nine')) {
-        if (display.textContent === '0') {
-            display.textContent = 9;
-        }
-        else if (display.textContent.length < 12 && display.textContent.length > 0) {
-            display.textContent += 9;
-        }
-        else {
-            remove.abort;
-        }
-    }
-    if (e.target.matches('#zero')) {
-        if (display.textContent === '0') {
-            display.textContent = 0;
-        }
-        else if (display.textContent.length < 12 && display.textContent.length > 0) {
-            display.textContent += 0;
-        }
-        else {
-            remove.abort;
-        }
-    }
+        dNum = Number(textNum)
+        operand2.textContent += textNum + ' / ';
+        operand1.textContent = '';
+    }   
 });
